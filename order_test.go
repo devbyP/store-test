1c366f23d410a8b54be48ecdd8c642d9eaf10cac
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2022-08-04 13:41:05 +0700

    test order

--- order_test.go
+++ order_test.go
@@ -8,3 +8,13 @@ func TestAddOrder(t *testing.T) {
 	testOrder := &order{}
 	orderStore.addOrder(testOrder)
 }
+
+func TestGetOrder(t *testing.T) {
+	testOrder := &order{}
+	id := orderStore.addOrder(testOrder)
+	order, err := orderStore.getOrder(id)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(order)
+}
